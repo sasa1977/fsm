2a5eb933244e45da7728ab127a3a851f1deddb72
Sasa Juric <sasa.juric@gmail.com>
2014-04-23 14:06:26 +0200

    hex manifest

--- mix.exs
+++ mix.exs
@@ -2,10 +2,20 @@ defmodule Fsm.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :fsm,
+    [
+      app: :fsm,
       version: "0.0.1",
       elixir: ">= 0.13.0",
-      deps: deps ]
+      deps: deps,
+      package: [
+        contributors: ["Saša Jurić"],
+        licenses: ["MIT"],
+        links: [{"Github", "https://github.com/sasa1977/fsm"}]
+      ],
+      description: """
+        Finite state machine as a functional data structure.
+      """
+    ]
   end
 
   def application do
